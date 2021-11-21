06ff385407b52a54652dc82e7b01ce4ad770b3e1
Ricardo Retamozo Garay <ricardo.retamozo@tecsup.edu.pe>
2021-11-20 19:04:58 -0500

    Initial Commit

---
+++ blogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  blogs
+//
+//  Created by ricardo on 11/20/21.
+//  Copyright © 2021 ricardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
